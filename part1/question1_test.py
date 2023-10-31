6d1a259296530b9b9991871fcb7a2f7cea53056a
Jmolinaro <jmolinaro@unal.edu.co>
2023-10-31 15:29:28 -0500

    first question done

--- part1/question1_test.py
+++ part1/question1_test.py
@@ -5,3 +5,8 @@ def test_get_city_weather():
   assert get_city_weather("Quito") == "22 degrees and sunny"
 
   assert get_city_weather("New York") == "14 degrees and rainy"
+
+  print("All tests passed!")
+
+if __name__ == "__main__":
+    test_get_city_weather()
